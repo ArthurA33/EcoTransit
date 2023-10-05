51af8bf7b51d155afba702ebfc5f5c87c955dea7
ArthurA33 <94461146+ArthurA33@users.noreply.github.com>
2023-10-05 07:23:45 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_5YQCxiyV4vAg32D_J6epTt7FR4GE5Gs&callback=initMap" async defer></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <script src="scripts/loadmap.js"></script>
     <script src="scripts/CreateRoad.js"></script>
     <title>Ecotransit</title>
@@ -12,10 +13,29 @@
     <a href="create.php">Créer un nouvel enregistrement</a>
     <div>
         <input type="text" id="start" placeholder="Point de départ">
-        <input type="text" id="end" placeholder="Point d'arrivée">
+        <input type="text" id="end" placeholder="Point d'arrivée"><br>
+
+        <select id="vehicleSelect" value="">
+            <option value="">Sélectionnez un véhicule</option>
+            <option value="VoitElec">Voiture électrique</option>
+            <option value="VoitTerm">Voiture thermique</option>
+            <option value="Velo">Vélo</option>
+            <option value="veloAssist">Vélo assister</option>
+            <option value="Marche">Marche</option>
+            <option value="BusTherm">Bus thermique</option>
+            <option value="BusElec">Bus électrique</option>
+            <option value="BusGNV">Bus GNV</option>
+            <option value="Tram">Tramway</option>
+            <option value="Metro">Métro</option>
+            <option value="Scoot">Scooter / moto légère</option>
+            <option value="Ter">TER</option>
+        </select><br><br>
+        
         <button onclick="calculateRoute()">Calculer l'itinéraire</button>
     </div>
     <div id="duration"></div>
+    <div id="distance"></div>
+    <div id="CO2"></div>
     <div id="google-map" style="width: 100%; height: 400px;"></div>
 </body>
 </html>
--- scripts/CreateRoad.js
+++ scripts/CreateRoad.js
@@ -20,15 +20,72 @@ function calculateRoute() {
   directionsService.route(request, function(response, status) {
     if (status === 'OK') {
       directionsDisplay.setDirections(response);
-      
-      // Récupérez la durée de l'itinéraire depuis la réponse
+
+      // Récupérez la durée et le nombre de kilometre de l'itinéraire depuis la réponse
       var route = response.routes[0];
       var duration = route.legs[0].duration.text;
+      var distance = route.legs[0].distance.value / 1000;
 
-      // Affichez la durée sur la page
-      document.getElementById('duration').innerHTML = 'Durée du trajet : ' + duration;
+      // Effectuez une requête vers l'API pour obtenir les données nécessaires
+      fetch("https://api.monimpacttransport.fr/beta/getEmissionsPerDistance")
+        .then(response => response.json())
+        .then(data => {
+          var veiculechoix = document.getElementById("vehicleSelect");
+          var valeurVeicule = veiculechoix.value;
+          var CO2cons;
+
+          var VoitElec = "VoitElec";
+          var VoitTerm = "VoitTerm";
+          var Velo = "Velo";
+          var veloAssist = "veloAssist";
+          var Marche = "Marche";
+          var BusTherm = "BusTherm";
+          var Tram = "Tram";
+          var Metro = "Metro";
+          var Scoot = "Scoot";
+          var Ter = "Ter";
+          var BusElec = "BusElec";
+          var BusGNV = "BusGNV";
+
+          if (valeurVeicule == VoitElec) {
+              CO2cons = data[0].emissions.gco2e;
+          } else if (valeurVeicule == VoitTerm) {
+              CO2cons = data[1].emissions.gco2e;
+          } else if (valeurVeicule == Velo || valeurVeicule == Marche) {
+              CO2cons = data[2].emissions.gco2e;
+          } else if (valeurVeicule == veloAssist) {
+              CO2cons = data[3].emissions.gco2e;
+          } else if (valeurVeicule == BusTherm) {
+              CO2cons = data[4].emissions.gco2e;
+          } else if (valeurVeicule == Tram) {
+              CO2cons = data[5].emissions.gco2e;
+          } else if (valeurVeicule == Metro) {
+              CO2cons = data[6].emissions.gco2e;
+          } else if (valeurVeicule == Scoot) {
+              CO2cons = data[7].emissions.gco2e;
+          } else if (valeurVeicule == Ter) {
+              CO2cons = data[8].emissions.gco2e;
+          } else if (valeurVeicule == BusElec) {
+              CO2cons = data[9].emissions.gco2e;
+          } else if (valeurVeicule == BusGNV) {
+              CO2cons = data[10].emissions.gco2e;
+          }
+          // calcule le CO2 utilisé en multipliant la distance par le nombre de CO2 en foction du veicule
+          var CO2_utiliser = distance.toFixed(1) * CO2cons;
+
+          document.getElementById('duration').innerHTML = 'Durée du trajet : ' + duration;
+          document.getElementById('distance').innerHTML = 'Nombre de kilomètres du trajet : ' + distance.toFixed(1);
+          document.getElementById('CO2').innerHTML = 'Nombre de CO2 utilisé : ' + CO2_utiliser.toFixed(2) + 'g';
+        })
+        .catch(error => {
+          console.error("Error fetching data:", error);
+        });
     } else {
       window.alert('Erreur de direction : ' + status);
     }
   });
 }
+
+
+
+
