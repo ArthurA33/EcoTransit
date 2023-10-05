9a3075200e989ad4a7b13431f67598447a3c363f
lightnew10 <64336831+lightnew10@users.noreply.github.com>
2023-10-05 14:07:49 +0200

    ajout page de contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,8 +5,31 @@ import './../style/contact.scss'
 const Contact = ()=>{
     return(
         <main>
-               <h1>contact</h1>
-
+            <section>
+                <div>
+                    <h1>Contactez nous via ce formulaire</h1>
+                </div>
+                <div className='parent-form'>
+                    <form action="/contact" method="post">
+                        <h2>Contact</h2>
+                        <div className='parent-content-form'>
+                            <label htmlFor="">Adresse mail</label>
+                            <input type="text" />
+                        </div>
+                        <div className='parent-content-form'>
+                            <label htmlFor="">Objet du message</label>
+                            <input type="text" />
+                        </div>
+                        <div className='parent-content-form'>
+                            <label htmlFor="">Message</label>
+                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                        </div>
+                        <div>
+                            <button className='button-from' type="submit">Envoyer votre message</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
         </main>     
     )
 }
